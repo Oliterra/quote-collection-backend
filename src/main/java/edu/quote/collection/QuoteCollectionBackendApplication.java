6d39c5816d2cfcd2d0c686ac95a0aecabc89c150
NadezhdaTarasova <tarasova_na_19@mf.grsu.by>
2023-02-28 22:18:58 +0300

    NAT-12 Created new spring boot project, add  initial dependencies

---
+++ src/main/java/edu/quote/collection/QuoteCollectionBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.quote.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteCollectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteCollectionBackendApplication.class, args);
+	}
+
+}
